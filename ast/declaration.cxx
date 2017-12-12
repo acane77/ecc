5fa9115a18ecce4ec111d13695277b4c5ddf10fd
Miyuki <moe@cpp.moe>
2017-12-12 10:30:36 +0800

    add constructors to declaration symbols

--- ast/declaration.cxx
+++ ast/declaration.cxx
@@ -2,4 +2,108 @@
 
 namespace Miyuki::AST {
 
+    Declaration::Declaration(const DeclarationSpecifiersPtr &decSpec, const InitDeclaratorListPtr &initDeclList)
+            : decSpec(decSpec), initDeclList(initDeclList) {}
+
+    DeclarationSpecifier::DeclarationSpecifier(const SpecifierAndQualifierPtr &spec,
+                                               const DeclarationSpecifierPtr &decSpec) : spec(spec), decSpec(decSpec) {}
+
+    StorageClassSpecifier::StorageClassSpecifier(const TokenPtr &tok) : tok(tok) {}
+
+    TypeSpecifier::TypeSpecifier(const TokenPtr &tok) : tok(tok) {}
+
+    TypeSpecifier::TypeSpecifier(const EnumSpecifierPtr &enumSpec) : enumSpec(enumSpec) {}
+
+    TypeSpecifier::TypeSpecifier(const StructOrUnionSpecifierPtr &structSpec) : structSpec(structSpec) {}
+
+    TypeQualifier::TypeQualifier(const TokenPtr &tok) : tok(tok) {}
+
+    FunctionSpecifier::FunctionSpecifier(const TokenPtr &tok) : tok(tok) {}
+
+    StructOrUnionSpecifier::StructOrUnionSpecifier(const TokenPtr &structOrUnion,
+                                                   const TokenPtr &id, const StructDeclarationListPtr &declList)
+            : TypeSpecifier(StructOrUnionSpecifierPtr(this)), structOrUnion(structOrUnion), id(id), declList(declList) {}
+
+    StructDeclaration::StructDeclaration(const SpecifierAndQualifierListPtr &specList,
+                                         const StructDeclaratorListPtr &structDecrList) : specList(specList),
+                                                                                          structDecrList(
+                                                                                                  structDecrList) {}
+
+    StructDeclarator::StructDeclarator(const DeclaratorPtr &decr, const ConstantExpressionPtr &constExpr) : decr(decr), constExpr(  constExpr) {}
+
+    EnumSpecifier::EnumSpecifier(const TokenPtr &id, const EnumeratorListPtr &enumList) : id(id), enumList(enumList) {}
+
+    Enumerator::Enumerator(const TokenPtr &enumConstant, const ConstantExpressionPtr &expr) : enumConstant(enumConstant), expr(expr) {}
+
+    Declarator::Declarator(const PointerDeclPtr &pointer, const DirectDeclaratorPtr &directDecl) : pointer(pointer), directDecl(directDecl) {}
+
+    Declarator::Declarator(const DirectDeclaratorPtr &directDecl) : directDecl(directDecl) {}
+
+    PointerDecl::PointerDecl(const TypeQualifierListPtr &typeQualList, const PointerDeclPtr &pointerDecl)
+            : typeQualList(typeQualList), pointerDecl(pointerDecl) {}
+
+    PointerDecl::PointerDecl(const PointerDeclPtr &pointerDecl) : pointerDecl(pointerDecl) {}
+
+    DirectDeclarator::DirectDeclarator(const TokenPtr &id) : id(id) { productionID = 0; }
+
+    DirectDeclarator::DirectDeclarator(const DirectDeclaratorPtr &directDecl, bool isStatic,
+                                       const AssignmentExpressionPtr &assignExpr,
+                                       const TypeQualifierListPtr &typeQualList, int productionID) : directDecl(
+            directDecl), isStatic(isStatic), assignExpr(assignExpr), typeQualList(typeQualList), productionID(
+            productionID) {}
+
+    DirectDeclarator::DirectDeclarator(const DirectDeclaratorPtr &directDecl, const TypeQualifierListPtr &typeQualList)
+            : directDecl(directDecl), typeQualList(typeQualList) { hasPointer = true; productionID = 5; }
+
+    DirectDeclarator::DirectDeclarator(const DirectDeclaratorPtr &directDecl, const WordTokenPtr &idList) : directDecl( directDecl), idList(idList) { productionID = 7; }
+
+    DirectDeclarator::DirectDeclarator(const DirectDeclaratorPtr &directDecl, const ParameterTypeListPtr &paramList)
+            : directDecl(directDecl), paramList(paramList) { productionID = 6; }
+
+    DirectDeclarator::DirectDeclarator(const DeclaratorPtr &decl) : decl(decl) { productionID = 1; }
+
+    ParameterTypeList::ParameterTypeList(const ParameterListPtr &paramList, bool isParameterVarible) : paramList( paramList), isParameterVarible(isParameterVarible) {}
+
+    ParameterDecleartion::ParameterDecleartion(const DeclarationSpecifierPtr &declSpec, const DeclaratorPtr &decr)
+            : declSpec(declSpec), decr(decr) {}
+
+    ParameterDecleartion::ParameterDecleartion(const DeclarationSpecifierPtr &declSpec, const AbstractDeclaratorPtr &abstructDecr)
+            : declSpec(declSpec), abstructDecr(abstructDecr) {}
+
+    AbstractDeclarator::AbstractDeclarator(const PointerDeclPtr &pointerDecl, const DirectAbstractDeclaratorPtr &directAbstractDecr)
+            : pointerDecl( pointerDecl), directAbstractDecr(directAbstractDecr) {}
+
+    TypeName::TypeName(const SpecifierAndQualifierListPtr &specList, const AbstractDeclaratorPtr &abstructDecr)
+            : specList(specList), abstructDecr(abstructDecr) {}
+
+    DirectAbstractDeclarator::DirectAbstractDeclarator(const AbstractDeclaratorPtr &abstracrDecr) : abstracrDecr(
+            abstracrDecr) { productionID = 0; }
+
+    DirectAbstractDeclarator::DirectAbstractDeclarator(const DirectAbstractDeclaratorPtr &directAbstractDecr,
+                                                       const AssignmentExpressionPtr &assignExpr,
+                                                       const TypeQualifierListPtr &typeQualList,
+                                                       bool  _isStatic, int _productionID)
+            : directAbstractDecr(directAbstractDecr), assignExpr(assignExpr), typeQualList(typeQualList), isStatic(_isStatic)
+    { productionID = _productionID; }
+
+    DirectAbstractDeclarator::DirectAbstractDeclarator(const DirectAbstractDeclaratorPtr &directAbstractDecr,
+                                                       const ParameterTypeListPtr &paramList) : directAbstractDecr(
+            directAbstractDecr), paramList(paramList) { productionID = 5; }
+
+    DirectAbstractDeclarator::DirectAbstractDeclarator(const DirectAbstractDeclaratorPtr &directAbstractDecr)
+            : directAbstractDecr(directAbstractDecr) { hasPointer = true; productionID = 4; }
+
+    Initializer::Initializer(const AssignmentExpressionPtr &assignExpr) : assignExpr(assignExpr) {}
+
+    Initializer::Initializer(const InitializerListPtr &initList) : initList(initList) {}
+
+    InitializerList::InitializerList(const InitializerPtr &init, const DesignationPtr &design,  const InitializerListPtr& initList) : init(init), design(design), initList(initList) {}
+
+    Designation::Designation(const DesignatorListPtr &desList) : desList(desList) {}
+
+    Designator::Designator(const ConstantExpressionPtr &constExpr) : constExpr(constExpr) {}
+
+    Designator::Designator(const WordTokenPtr &id) : id(id) {}
+
+    InitDeclarator::InitDeclarator(const DesignatorPtr &desOr, const InitializerPtr &init) : desOr(desOr), init(init) {}
 }
--- ast/declaration.h
+++ ast/declaration.h
@@ -6,7 +6,7 @@
 
 // Declaration contains compile-time information
 
-// ref ISO/IEC 9899:201x ¡ìA.2.2
+// ref ISO/IEC 9899:201x ï¿½ï¿½A.2.2
 
 namespace Miyuki::AST {
 
@@ -17,43 +17,43 @@ namespace Miyuki::AST {
 #define DEFINE_LIST(type) typedef deque<type> type##List; typedef shared_ptr<type##List> type##ListPtr;
 
     DEFINE_SHARED_PTR(IDeclaration)
-        DEFINE_SHARED_PTR(Declaration)
-        DEFINE_SHARED_PTR(DeclarationSpecifiers)
-        DEFINE_SHARED_PTR(InitDeclarator)
-        DEFINE_LIST(InitDeclarator)
-        DEFINE_SHARED_PTR(DeclarationSpecifier)
-        DEFINE_SHARED_PTR(SpecifierAndQualifier)
-        DEFINE_LIST(SpecifierAndQualifier)
-        DEFINE_SHARED_PTR(StorageClassSpecifier)
-        DEFINE_SHARED_PTR(TypeSpecifier)
-        DEFINE_SHARED_PTR(TypeQualifier)
-        DEFINE_LIST(TypeQualifier)
-        DEFINE_SHARED_PTR(StructOrUnionSpecifier)
-        DEFINE_SHARED_PTR(StructDeclaration)
-        DEFINE_LIST(StructDeclaration)
-        DEFINE_SHARED_PTR(StructDeclarator)
-        DEFINE_LIST(StructDeclarator)
-        DEFINE_SHARED_PTR(Declarator)
-        DEFINE_SHARED_PTR(EnumSpecifier)
-        DEFINE_SHARED_PTR(Enumerator)
-        DEFINE_LIST(Enumerator)
-        DEFINE_SHARED_PTR(PointerDecl)
-        DEFINE_SHARED_PTR(DirectDeclarator)
-        DEFINE_SHARED_PTR(Parameter)
-        DEFINE_LIST(WordToken)
-        DEFINE_SHARED_PTR(ParameterTypeList)
-        DEFINE_SHARED_PTR(ParameterDecleartion)
-        DEFINE_LIST(ParameterDecleartion)
-        typedef ParameterDecleartionList ParameterList;
-        typedef ParameterDecleartionListPtr ParameterListPtr;
-        DEFINE_SHARED_PTR(AbstractDeclarator)
-        DEFINE_SHARED_PTR(DirectAbstractDeclarator)
-        DEFINE_SHARED_PTR(TypeName)
-        DEFINE_SHARED_PTR(Initializer)
-        DEFINE_SHARED_PTR(InitializerList)
-        DEFINE_SHARED_PTR(Designation)
-        DEFINE_SHARED_PTR(Designator)
-        DEFINE_LIST(Designator)
+    DEFINE_SHARED_PTR(Declaration)
+    DEFINE_SHARED_PTR(DeclarationSpecifiers)
+    DEFINE_SHARED_PTR(InitDeclarator)
+    DEFINE_LIST(InitDeclarator)
+    DEFINE_SHARED_PTR(DeclarationSpecifier)
+    DEFINE_SHARED_PTR(SpecifierAndQualifier)
+    DEFINE_LIST(SpecifierAndQualifier)
+    DEFINE_SHARED_PTR(StorageClassSpecifier)
+    DEFINE_SHARED_PTR(TypeSpecifier)
+    DEFINE_SHARED_PTR(TypeQualifier)
+    DEFINE_LIST(TypeQualifier)
+    DEFINE_SHARED_PTR(StructOrUnionSpecifier)
+    DEFINE_SHARED_PTR(StructDeclaration)
+    DEFINE_LIST(StructDeclaration)
+    DEFINE_SHARED_PTR(StructDeclarator)
+    DEFINE_LIST(StructDeclarator)
+    DEFINE_SHARED_PTR(Declarator)
+    DEFINE_SHARED_PTR(EnumSpecifier)
+    DEFINE_SHARED_PTR(Enumerator)
+    DEFINE_LIST(Enumerator)
+    DEFINE_SHARED_PTR(PointerDecl)
+    DEFINE_SHARED_PTR(DirectDeclarator)
+    DEFINE_SHARED_PTR(Parameter)
+    DEFINE_LIST(WordToken)
+    DEFINE_SHARED_PTR(ParameterTypeList)
+    DEFINE_SHARED_PTR(ParameterDecleartion)
+    DEFINE_LIST(ParameterDecleartion)
+    typedef ParameterDecleartionList ParameterList;
+    typedef ParameterDecleartionListPtr ParameterListPtr;
+    DEFINE_SHARED_PTR(AbstractDeclarator)
+    DEFINE_SHARED_PTR(DirectAbstractDeclarator)
+    DEFINE_SHARED_PTR(TypeName)
+    DEFINE_SHARED_PTR(Initializer)
+    DEFINE_SHARED_PTR(InitializerList)
+    DEFINE_SHARED_PTR(Designation)
+    DEFINE_SHARED_PTR(Designator)
+    DEFINE_LIST(Designator)
 
     class IDeclaration : public Symbol {
     public:
@@ -62,8 +62,10 @@ namespace Miyuki::AST {
 
     class Declaration : public IDeclaration {
     public:
-        DeclarationSpecifiersPtr decSpec;
-        InitDeclaratorListPtr    initDeclList;
+        DeclarationSpecifiersPtr decSpec = nullptr;
+        InitDeclaratorListPtr    initDeclList = nullptr;
+
+        Declaration(const DeclarationSpecifiersPtr &decSpec, const InitDeclaratorListPtr &initDeclList);
 
         // we do not support static_assert
         int getKind() override { return Kind::declaration; }
@@ -71,8 +73,11 @@ namespace Miyuki::AST {
     };
 
     class DeclarationSpecifier : public IDeclaration {
-        SpecifierAndQualifierPtr spec;
-        DeclarationSpecifierPtr  decSpec;
+    public:
+        SpecifierAndQualifierPtr spec = nullptr;
+        DeclarationSpecifierPtr  decSpec = nullptr;
+
+        DeclarationSpecifier(const SpecifierAndQualifierPtr &spec, const DeclarationSpecifierPtr &decSpec);
 
         int getKind() override { return Kind::declarationSpecifier; }
         void gen() override;
@@ -80,45 +85,62 @@ namespace Miyuki::AST {
 
     class SpecifierAndQualifier : public IDeclaration {
         virtual int getKind() { assert( false && "unimplemented" ); }
-        void gen() override;
+        virtual void gen() { assert ( false && "unimplemented" ); }
     };
 
     class StorageClassSpecifier : public SpecifierAndQualifier {
-        TokenPtr tok;
+    public:
+        TokenPtr tok = nullptr;
+
+        explicit StorageClassSpecifier(const TokenPtr &tok);
 
         virtual int getKind() { return Kind::storageClassSpecifier; }
         void gen() override;
     };
 
     class TypeSpecifier : public SpecifierAndQualifier {
-        TokenPtr tok; // INCLUDE typedef-name
+    public:
+        TokenPtr tok = nullptr; // INCLUDE typedef-name
         // OR
-        StructOrUnionSpecifierPtr structSpec;
+        StructOrUnionSpecifierPtr structSpec = nullptr;
         // OR
-        EnumSpecifierPtr enumSpec;
+        EnumSpecifierPtr enumSpec = nullptr;
+
+        explicit TypeSpecifier(const EnumSpecifierPtr &enumSpec);
+        explicit TypeSpecifier(const TokenPtr &tok);
+        explicit TypeSpecifier(const StructOrUnionSpecifierPtr &structSpec);
 
         virtual int getKind() { return Kind::typeSpecifier; }
-        void gen() override;
+        virtual void gen() { assert ( false && "unimplemented" ); }
     };
 
     class TypeQualifier : public SpecifierAndQualifier {
-        TokenPtr tok;
+    public:
+        TokenPtr tok = nullptr;
+
+        explicit TypeQualifier(const TokenPtr &tok);
 
         virtual int getKind() { return Kind::typeQualifier; }
         void gen() override;
     };
 
     class FunctionSpecifier : public SpecifierAndQualifier {
-        TokenPtr tok;
+    public:
+        TokenPtr tok = nullptr;
+
+        explicit FunctionSpecifier(const TokenPtr &tok);
 
         virtual int getKind() { return Kind::functionSpecifier; }
         void gen() override;
     };
 
     class StructOrUnionSpecifier : public TypeSpecifier {
-        TokenPtr structOrUnion;
-        TokenPtr id;
-        StructDeclarationListPtr declList;
+    public:
+        TokenPtr structOrUnion = nullptr;
+        TokenPtr id = nullptr;
+        StructDeclarationListPtr declList = nullptr;
+
+        StructOrUnionSpecifier(const TokenPtr &structOrUnion, const TokenPtr &id, const StructDeclarationListPtr &declList);
 
         virtual int getKind() { return Kind::structOrUninSpecifier; }
         void gen() override;
@@ -126,9 +148,10 @@ namespace Miyuki::AST {
 
     class StructDeclaration: public IDeclaration {
     public:
-        SpecifierAndQualifierPtr spec;
-        StructDeclarationPtr     structDecl;
-        StructDeclaratorListPtr  structDecrList;
+        SpecifierAndQualifierListPtr specList = nullptr;
+        StructDeclaratorListPtr  structDecrList = nullptr;
+
+        StructDeclaration(const SpecifierAndQualifierListPtr &specList, const StructDeclaratorListPtr &structDecrList);
 
         virtual int getKind() { return Kind::structDeclaration; }
         void gen() override;
@@ -136,8 +159,10 @@ namespace Miyuki::AST {
 
     class StructDeclarator : public IDeclaration {
     public:
-        DeclaratorPtr  decr;
-        ConstantExpressionPtr constExpr;
+        DeclaratorPtr  decr = nullptr;
+        ConstantExpressionPtr constExpr = nullptr;
+
+        explicit StructDeclarator(const DeclaratorPtr &decr, const ConstantExpressionPtr &constExpr = nullptr);
 
         virtual int getKind() { return Kind::structDeclarator; }
         void gen() override;
@@ -145,8 +170,10 @@ namespace Miyuki::AST {
 
     class EnumSpecifier : public IDeclaration {
     public:
-        TokenPtr id;
-        EnumeratorListPtr enumList;
+        TokenPtr id = nullptr;
+        EnumeratorListPtr enumList = nullptr;
+
+        EnumSpecifier(const TokenPtr &id, const EnumeratorListPtr &enumList);
 
         virtual int getKind() { return Kind::enumSpecifier; }
         void gen() override;
@@ -154,8 +181,10 @@ namespace Miyuki::AST {
 
     class Enumerator : public IDeclaration {
     public:
-        TokenPtr enumConstant;
-        ConstantExpressionPtr expr;
+        TokenPtr enumConstant = nullptr;
+        ConstantExpressionPtr expr = nullptr;
+
+        explicit Enumerator(const TokenPtr &enumConstant, const ConstantExpressionPtr &expr = nullptr);
 
         virtual int getKind() { return Kind::enumerator; }
         void gen() override;
@@ -163,8 +192,12 @@ namespace Miyuki::AST {
 
     class Declarator : public IDeclaration {
     public:
-        PointerDeclPtr pointer;
-        DirectDeclaratorPtr directDecl;
+        PointerDeclPtr pointer = nullptr;
+        DirectDeclaratorPtr directDecl = nullptr;
+
+        explicit Declarator(const DirectDeclaratorPtr &directDecl);
+        Declarator(const PointerDeclPtr &pointer, const DirectDeclaratorPtr &directDecl);
+
 
         virtual int getKind() { return Kind::declarator; }
         void gen() override;
@@ -172,8 +205,11 @@ namespace Miyuki::AST {
 
     class PointerDecl : public IDeclaration {
     public:
-        TypeQualifierListPtr typeQualList;
-        PointerDeclPtr pointerDecl;
+        TypeQualifierListPtr typeQualList = nullptr;
+        PointerDeclPtr pointerDecl = nullptr;
+
+        PointerDecl(const TypeQualifierListPtr &typeQualList, const PointerDeclPtr &pointerDecl);
+        PointerDecl(const PointerDeclPtr &pointerDecl);
 
         virtual int getKind() { return Kind::pointer_decl; }
         void gen() override;
@@ -181,17 +217,29 @@ namespace Miyuki::AST {
 
     class DirectDeclarator : public IDeclaration {
     public:
-        TokenPtr id;
+        TokenPtr id = nullptr;
         // OR
-        DeclaratorPtr decl;
+        DeclaratorPtr decl = nullptr;
         // OR
-        DirectDeclaratorPtr directDecl;
+        DirectDeclaratorPtr directDecl = nullptr;
         bool isStatic = false;
-        AssignmentExpressionPtr assignExpr;
+        AssignmentExpressionPtr assignExpr = nullptr;
         bool hasPointer = false;
-        ParameterTypeListPtr paramList;
-        WordTokenPtr idList;
-        TypeQualifierListPtr typeQualList;
+        ParameterTypeListPtr paramList = nullptr;
+        WordTokenPtr idList = nullptr;
+        TypeQualifierListPtr typeQualList = nullptr;
+
+        // production to be used (ref 6.7.6)
+        int productionID;
+
+        explicit DirectDeclarator(const TokenPtr &id);
+        explicit DirectDeclarator(const DeclaratorPtr &decl);
+        DirectDeclarator(const DirectDeclaratorPtr &directDecl, bool isStatic,
+                         const AssignmentExpressionPtr &assignExpr, const TypeQualifierListPtr &typeQualList,
+                         int productionID = 2);
+        DirectDeclarator(const DirectDeclaratorPtr &directDecl, const TypeQualifierListPtr &typeQualList);
+        DirectDeclarator(const DirectDeclaratorPtr &directDecl, const ParameterTypeListPtr &paramList);
+        DirectDeclarator(const DirectDeclaratorPtr &directDecl, const WordTokenPtr &idList);
 
         virtual int getKind() { return Kind::directDeclarator; }
         void gen() override;
@@ -199,8 +247,10 @@ namespace Miyuki::AST {
 
     class ParameterTypeList : public IDeclaration {
     public:
-        ParameterListPtr paramList;
+        ParameterListPtr paramList = nullptr;
         bool isParameterVarible;
+
+        explicit ParameterTypeList(const ParameterListPtr &paramList, bool isParameterVarible = false);
 
         virtual int getKind() { return Kind::parameterTypeList; }
         void gen() override;
@@ -208,11 +258,15 @@ namespace Miyuki::AST {
 
     class ParameterDecleartion : public IDeclaration {
     public:
-        DeclarationSpecifierPtr declSpec;
+        DeclarationSpecifierPtr declSpec = nullptr;
         
-        DeclaratorPtr decr;
+        DeclaratorPtr decr = nullptr;
         //OR
-        AbstractDeclaratorPtr abstructDecr;
+        AbstractDeclaratorPtr abstructDecr = nullptr;
+
+        ParameterDecleartion(const DeclarationSpecifierPtr &declSpec, const DeclaratorPtr &decr);
+
+        explicit ParameterDecleartion(const DeclarationSpecifierPtr &declSpec, const AbstractDeclaratorPtr &abstructDecr = nullptr);
 
         virtual int getKind() { return Kind::parameterDecleartion; }
         void gen() override;
@@ -220,8 +274,10 @@ namespace Miyuki::AST {
 
     class AbstractDeclarator : public IDeclaration {
     public:
-        PointerDeclPtr pointerDecl;
-        DirectAbstractDeclaratorPtr directAbstractDecr;
+        PointerDeclPtr pointerDecl = nullptr;
+        DirectAbstractDeclaratorPtr directAbstractDecr = nullptr;
+
+        AbstractDeclarator(const PointerDeclPtr &pointerDecl, const DirectAbstractDeclaratorPtr &directAbstractDecr);
 
         virtual int getKind() { return Kind::abstractDeclarator; }
         void gen() override;
@@ -229,8 +285,10 @@ namespace Miyuki::AST {
 
     class TypeName : public IDeclaration {
     public:
-        SpecifierAndQualifierListPtr specList;
-        AbstractDeclaratorPtr abstructDecr;
+        SpecifierAndQualifierListPtr specList = nullptr;
+        AbstractDeclaratorPtr abstructDecr = nullptr;
+
+        TypeName(const SpecifierAndQualifierListPtr &specList, const AbstractDeclaratorPtr &abstructDecr);
 
         virtual int getKind() { return Kind::typeName; }
         void gen() override;
@@ -238,14 +296,23 @@ namespace Miyuki::AST {
 
     class DirectAbstractDeclarator : public IDeclaration {
     public:
-        AbstractDeclaratorPtr abstracrDecr;
+        AbstractDeclaratorPtr abstracrDecr = nullptr;
         // OR
-        DirectAbstractDeclaratorPtr directAbstractDecr;
+        DirectAbstractDeclaratorPtr directAbstractDecr = nullptr;
         bool isStatic = false;
-        AssignmentExpressionPtr assignExpr;
+        AssignmentExpressionPtr assignExpr = nullptr;
         bool hasPointer = false;
-        ParameterTypeListPtr paramList;
-        TypeQualifierListPtr typeQualList;
+        ParameterTypeListPtr paramList = nullptr;
+        TypeQualifierListPtr typeQualList = nullptr;
+
+        // ref 6.7.7   production ID
+        int productionID;
+
+        explicit DirectAbstractDeclarator(const AbstractDeclaratorPtr &abstracrDecr);
+        DirectAbstractDeclarator(const DirectAbstractDeclaratorPtr &directAbstractDecr,
+                                 const AssignmentExpressionPtr &assignExpr, const TypeQualifierListPtr &typeQualList, bool _isStatic, int _productionID = 1);
+        explicit DirectAbstractDeclarator(const DirectAbstractDeclaratorPtr &directAbstractDecr);
+        DirectAbstractDeclarator(const DirectAbstractDeclaratorPtr &directAbstractDecr, const ParameterTypeListPtr &paramList);
 
         virtual int getKind() { return Kind::directAbstractDeclarator; }
         void gen() override;
@@ -253,9 +320,12 @@ namespace Miyuki::AST {
 
     class Initializer : public IDeclaration {
     public:
-        AssignmentExpressionPtr assignExpr;
+        AssignmentExpressionPtr assignExpr = nullptr;
         // OR
-        InitializerListPtr      initList;
+        InitializerListPtr      initList = nullptr;
+
+        explicit Initializer(const AssignmentExpressionPtr &assignExpr);
+        explicit Initializer(const InitializerListPtr &initList);
 
         virtual int getKind() { return Kind::init; }
         void gen() override;
@@ -263,9 +333,11 @@ namespace Miyuki::AST {
 
     class InitializerList : public IDeclaration {
     public:
-        InitializerListPtr  initList;
-        Initializer         init;
-        DesignationPtr      design;
+        InitializerListPtr  initList = nullptr;
+        InitializerPtr      init = nullptr;
+        DesignationPtr      design = nullptr;
+
+        InitializerList(const InitializerPtr &init, const DesignationPtr &design, const InitializerListPtr& initList = nullptr);
 
         virtual int getKind() { return Kind::initList; }
         void gen() override;
@@ -273,7 +345,9 @@ namespace Miyuki::AST {
 
     class Designation : public IDeclaration {
     public:
-        DesignatorListPtr desList;
+        DesignatorListPtr desList = nullptr;
+
+        explicit Designation(const DesignatorListPtr &desList);
 
         virtual int getKind() { return Kind::designation; }
         void gen() override;
@@ -281,9 +355,12 @@ namespace Miyuki::AST {
 
     class Designator : public IDeclaration {
     public:
-        ConstantExpressionPtr constExpr;
+        ConstantExpressionPtr constExpr = nullptr;
         // OR
-        WordTokenPtr id;
+        WordTokenPtr id = nullptr;
+
+        explicit Designator(const ConstantExpressionPtr &constExpr);
+        explicit Designator(const WordTokenPtr &id);
 
         virtual int getKind() { return Kind::designator; }
         void gen() override;
@@ -291,8 +368,10 @@ namespace Miyuki::AST {
 
     class InitDeclarator : public IDeclaration {
     public:
-        DesignatorPtr desOr;
-        Initializer   init;
+        DesignatorPtr desOr = nullptr;
+        InitializerPtr init = nullptr;
+
+        explicit InitDeclarator(const DesignatorPtr &desOr, const InitializerPtr &init = nullptr);
 
         virtual int getKind() { return Kind::initDeclr; }
         void gen() override;
